8abfec1b5bb8c4b1444b88fc96cc0d00629dfc28
Kevin M <k.mary@live.be>
2024-07-04 13:56:26 +0200

    founir le service via le provider du component parent rendant dispo uniquement aux enfants

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+// import { TasksService } from './app/tasks/tasks.service';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// Il est possible de passer des providers à bootstrapApplication pour fournir des services à l'application entière (moins conseillé)
+// bootstrapApplication(AppComponent, {
+//     providers: [TasksService],
+// }).catch((err) => console.error(err));
